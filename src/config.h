2e623458facaa0b37260ef487f6cfabee8001568
Kevin C <kevcdragon@gmail.com>
2025-02-15 20:06:42 -0500

    init

--- src/config.h
+++ src/config.h
@@ -10,5 +10,4 @@
 #define WINDOW_WIDTH (MAP_WIDTH * SCALED_TILE_SIZE)
 #define WINDOW_HEIGHT (MAP_HEIGHT * SCALED_TILE_SIZE)
 
-#define GLOBAL_SPRITE_SHEET_PATH                                               \
-  "E:/Kevin/Documents/c projects/RoguelySDL3/assets/monochrome_packed.png"
+#define GLOBAL_SPRITE_SHEET_PATH "/assets/monochrome_packed.png"
